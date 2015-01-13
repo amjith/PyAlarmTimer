f664daa42d9ec70fc7ac512ce71868c703e8a011
Amjith Ramanujam <amjith.r@gmail.com>
2015-01-12 16:32:57 -0800

    Add a LICENSE (MIT).

--- setup_win.py
+++ setup_win.py
@@ -5,8 +5,8 @@ sys.argv.append('py2exe')
 setup(
     options = {'py2exe':{'bundle_files':1, 'includes':["sip"]}},
     windows = [{'script': "AlarmSetup.py"}],
-    license="GNU General Public License (GPL)",  
-    package_data={"AlarmClock": ["Resources/bell.png"]},  
+    license="MIT",
+    package_data={"AlarmClock": ["Resources/bell.png"]},
 
     zipfile = None,
     )
